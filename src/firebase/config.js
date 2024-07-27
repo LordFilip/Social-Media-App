54d89ca0395ad0af2018b88b9a377e2fc389f5f0
Filip <filipivanovic59@gmail.com>
2024-07-27 12:51:01 +0200

    main navbar created and started with styling

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,6 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAeRShdlIhXFY1lhNhpduyhYSxhy-Ymwd4',
-  authDomain: 'social-media-app-fafb8.firebaseapp.com',
-  projectId: 'social-media-app-fafb8',
-  storageBucket: 'social-media-app-fafb8.appspot.com',
-  messagingSenderId: '70598245159',
-  appId: '1:70598245159:web:b138c6010c243cc7ca8702',
-}
-
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const firestore = getFirestore(app)

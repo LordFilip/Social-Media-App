c72450a91ca602afaabcc94ebc552f243d09b8f1
Filip <filipivanovic59@gmail.com>
2024-07-27 13:19:29 +0200

    started styling of profilepage

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,13 +1,20 @@
 import Navbar from '../components/Navbar/Navbar'
 import useGetPosts from '../hooks/useGetPosts'
 import styles from './Styles/ProfilePage.module.css'
+import image from '../../public/assets/auth-image.jpg'
 
 const ProfilePage = () => {
   const { posts, loading, error } = useGetPosts()
 
   return (
-    <div className={styles.profilePage}>
+    <div className={styles.profilePageContainer}>
       <Navbar />
+      <div className={styles.coverPhoto}>
+        <img src={image} alt="" />
+        <div className={styles.profilePhoto}>
+          <img src={image} alt="" />
+        </div>
+      </div>
       <h1>Profile Page</h1>
       {loading && <p>Loading posts...</p>}
       {error && <p className={styles.error}>{error}</p>}
--- src/components/HomePage/RightSidebar.jsx
+++ src/components/HomePage/RightSidebar.jsx
@@ -4,6 +4,9 @@ const RightSidebar = () => {
   return (
     <div className={styles.RightSidebar}>
       <input type="text" />
+      <h1>asdasd</h1>
+      <h1>asdasd</h1>
+      <h1>asdasd</h1>
     </div>
   )
 }

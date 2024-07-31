68f469d61f05a7afa27160dbb8c7e10bfecf7eb4
Filip <filipivanovic59@gmail.com>
2024-07-31 17:17:27 +0200

    icons

--- src/components/ProfilePage/ProfilePost/ProfilePost.jsx
+++ src/components/ProfilePage/ProfilePost/ProfilePost.jsx
@@ -1,9 +1,15 @@
 import { formatDistanceToNow, parseISO } from 'date-fns'
 import styles from './ProfilePost.module.css'
+import { FaRegHeart } from 'react-icons/fa'
+import { FaRegCommentDots } from 'react-icons/fa'
+import { FaRegShareSquare } from 'react-icons/fa'
+import { FcLike } from 'react-icons/fc'
+import { useState } from 'react'
 
 const ProfilePost = ({ text, time, username }) => {
-  // Convert time to a human-readable format
   const formattedTime = formatDistanceToNow(parseISO(time), { addSuffix: true })
+
+  const [isLiked, setIsLiked] = useState(false)
 
   return (
     <div className={styles.postContainer}>
@@ -13,6 +19,15 @@ const ProfilePost = ({ text, time, username }) => {
       </div>
       <div className={styles.postMain}>
         <p>{text}</p>
+      </div>
+      <div className={styles.likeRow}>
+        {isLiked ? (
+          <FcLike className={styles.icon} />
+        ) : (
+          <FaRegHeart className={styles.icon} />
+        )}
+        <FaRegCommentDots className={styles.icon} />
+        <FaRegShareSquare className={styles.icon} />
       </div>
     </div>
   )
